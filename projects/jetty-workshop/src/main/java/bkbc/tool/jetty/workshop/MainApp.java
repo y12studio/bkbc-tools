c58e6bdd94272bf443af1b9e875e12742ece4378
y12studio <y12studio@gmail.com>
2014-04-20 18:34:17 +0800

    print docker image ip #27

--- projects/jetty-workshop/src/main/java/bkbc/tool/jetty/workshop/MainApp.java
+++ projects/jetty-workshop/src/main/java/bkbc/tool/jetty/workshop/MainApp.java
@@ -15,10 +15,34 @@
  */
 package bkbc.tool.jetty.workshop;
 
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
+
+import com.google.common.net.InetAddresses;
+
 public class MainApp {
+	
+	public static void printNetworkIp() throws SocketException{
+		Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
+		while (interfaces.hasMoreElements()){
+		    NetworkInterface current = interfaces.nextElement();
+		    //System.out.println(current);
+		    if (!current.isUp() || current.isLoopback() || current.isVirtual()) continue;
+		    Enumeration<InetAddress> addresses = current.getInetAddresses();
+		    while (addresses.hasMoreElements()){
+		        InetAddress addr = addresses.nextElement();
+		        if(InetAddresses.isInetAddress(addr.getHostAddress())){
+		        	System.out.println("IP:"+addr.getHostAddress());		        	
+		        }
+		    }
+		}
+	}
 
 	public static void main(String[] args) {
 		try {
+			printNetworkIp();
 			new JettyCrosServer().start();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
